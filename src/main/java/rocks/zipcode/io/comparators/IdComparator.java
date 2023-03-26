569a37144ffdcdd3fc8e6b72dab063f6d398bfd9
Nina <nina.emiko.mcnair@gmail.com>
2023-03-26 18:53:03 -0400

    All tests passing

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,16 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item a, Item b) {
+        return (int)(a.getId() - b.getId());
+    }
+
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,22 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+
+    @Override
+    public int compare(Item a, Item b) {
+        if (a.getPrice() > b.getPrice()) {
+            return 1;
+        } else if (a.getPrice() < b.getPrice()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
